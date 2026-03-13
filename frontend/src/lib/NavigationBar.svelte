167467eaf0947f4d00bc37f605f45c737ab1dde4
Rebecca Prechtl <rlpmuc2006@gmail.com>
2026-03-13 20:52:37 +0100

    Add NavigationBar

---
+++ frontend/src/lib/NavigationBar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+    import {Search} from '@lucide/svelte'
+    import {CircleUserRound} from '@lucide/svelte'
+    import {House} from '@lucide/svelte'
+    import {LogIn} from "@lucide/svelte";
+    import {goto} from "$app/navigation";
+
+    function navHome(){
+        goto("/")
+    }
+
+    function navSearch() {
+        goto("/search")
+    }
+
+    function navProfile() {
+        goto("/profile")
+    }
+
+    function navLogin() {
+        goto("/login")
+    }
+
+
+</script>
+
+<button type="button" on:click={navHome}>
+    <House/>
+</button>
+<button type="button" on:click={navSearch}>
+    <Search/>
+</button>
+<button type="button" on:click={navProfile}>
+    <CircleUserRound />
+</button>
+<button type="button" on:click={navLogin}>
+    <LogIn />
+</button>
+
+
+
