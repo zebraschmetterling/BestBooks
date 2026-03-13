71bf381a3043781fff825257d8e18e1f00e44083
Rebecca Prechtl <rlpmuc2006@gmail.com>
2026-03-13 20:57:09 +0100

    Add navbar to home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import NavigationBar from "$lib/NavigationBar.svelte";
+</script>
+
+<h1>home</h1>
+<NavigationBar/>
