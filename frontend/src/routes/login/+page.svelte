8a1591a91b6137f2b60b96a9f0747431fae920ce
Rebecca Prechtl <rlpmuc2006@gmail.com>
2026-03-13 20:55:05 +0100

    Add login & register page form, not working yet

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+    import NavigationBar from "$lib/NavigationBar.svelte";
+
+
+</script>
+
+<h2 class="header">
+    Login
+</h2>
+
+<form method="post" >
+    <label>
+        Username
+        <input
+            name="username"
+            type="text"
+            placeholder="username"
+        />
+    </label>
+    <br>
+    <label>
+        Password
+        <input
+                name="password"
+                type="password"
+                placeholder="password"
+        />
+    </label>
+    <br>
+    <input
+            name="submit"
+            type="submit"
+            value="submit"
+    />
+</form>
+
+<h4>
+    New here? Then register <a href="/register"> here </a>.
+</h4>
+
+<NavigationBar/>
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+
+
+import NavigationBar from "$lib/NavigationBar.svelte";
+</script>
+
+<h2 class="header">
+    Register
+</h2>
+
+<form method="post" >
+    <label>
+        Username
+        <input
+                name="username"
+                type="text"
+                placeholder="username"
+        />
+    </label>
+    <br>
+    <label>
+        Password
+        <input
+                name="password"
+                type="password"
+                placeholder="password"
+        />
+    </label>
+    <br>
+    <label>
+        Password Check
+        <input
+                name="password check"
+                type="password"
+                placeholder="repeat password"
+        />
+    </label>
+    <br>
+    <input
+            name="submit"
+            type="submit"
+            value="submit"
+    />
+</form>
+
+<h4>
+    Already have an account? Then login <a href="/login"> here </a>.
+</h4>
+<NavigationBar/>
