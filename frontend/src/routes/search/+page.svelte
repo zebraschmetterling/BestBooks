3d7198357bb5701f579274d00e465a049c3a806c
Rebecca Prechtl <rlpmuc2006@gmail.com>
2026-03-13 20:55:47 +0100

    Add search page form, not working yet

---
+++ frontend/src/routes/search/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+    import NavigationBar from "$lib/NavigationBar.svelte";
+    import type {Actions} from "@sveltejs/kit";
+
+    async function fetch_data (q:string) {
+        await fetch ("http://localhost:8080/search")
+    }
+
+    export const actions = {
+        search: async (event) => {
+            await fetch("http://localhost:8080/search")
+        }
+    } satisfies Actions
+
+</script>
+
+<h1> Search</h1>
+<form method="POST" action="/search">
+    <input
+            name="searchbar"
+            type="text"
+            placeholder="Search your book"
+    />
+
+</form>
+<br>
+
+
+
+<NavigationBar/>
