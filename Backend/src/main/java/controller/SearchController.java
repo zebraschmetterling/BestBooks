f61bd7521178e0216450c96921d295cbf0126b2a
Rebecca Prechtl <rlpmuc2006@gmail.com>
2026-03-19 13:37:50 +0100

    Add search controller

---
+++ Backend/src/main/java/controller/SearchController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import api.GoogleBooksApi;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/search")
+public class SearchController {
+
+    private final GoogleBooksApi api;
+
+    @Autowired
+    public SearchController(GoogleBooksApi api) {
+        this.api = api;
+    }
+
+    @GetMapping("/{q}")
+    public JSONObject search(@PathVariable String q){
+        return api.search(q);
+    }
+
+}
